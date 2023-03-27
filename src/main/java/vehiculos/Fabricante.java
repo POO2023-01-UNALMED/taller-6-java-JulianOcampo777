f45f5c92120f462f7b981435c4af55d1a87a823c
JulianOcampo777 <124713530+JulianOcampo777@users.noreply.github.com>
2023-03-27 16:35:24 -0500

    TS129

---
+++ src/main/java/vehiculos/Fabricante.java
@@ -0,0 +1,19 @@
+package vehiculos;
+
+public class Fabricante {
+	private String nombre;
+	private Pais pais;
+	public Pais getPais() {
+		return pais;
+	}
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+}
---
+++ src/main/java/vehiculos/Pais.java
@@ -0,0 +1,14 @@
+package vehiculos;
+
+public class Pais {
+	private String nombre;
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+}
